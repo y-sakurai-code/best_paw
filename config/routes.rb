de459808b98e94c79f277f7f144bc5ba6210b1f0
y-sakurai <235877267+y-sakurai-code@users.noreply.github.com>
2025-12-28 13:43:17 +0000

    upadate/top&about.html devise/new.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/about'
+  devise_for :users
+  root to: 'homes#top'
+  et 'homes/about', to: 'homes#about', as: :about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

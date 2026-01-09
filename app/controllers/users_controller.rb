1664a5d89b9b6ead8c9088d6e79f49c25ffe3ac0
y-sakurai <235877267+y-sakurai-code@users.noreply.github.com>
2026-01-09 12:09:46 +0000

    update/mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+
   def mypage
+    @user = current_user
+    @reviews = Review.where(user_id: current_user.id)
   end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def archive
+  end
+
 end

b4794ecf59d155a05ab6a7580da53d934f7f7e24
y-sakurai <235877267+y-sakurai-code@users.noreply.github.com>
2026-01-10 11:49:05 +0000

    update/users_mypage.html & edit.html & show.html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,32 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path(current_user.id), notice: "更新しました"
+    else
+      render :edit
+    end
   end
 
   def show
+    @user = current_user
   end
 
   def archive
+    user = User.find(params[:id])
+    user.archive
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 
 end

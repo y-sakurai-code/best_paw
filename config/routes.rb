ffc5a16d3e2cbe69c5c05bde4c371c009cd9704e
y-sakurai <235877267+y-sakurai-code@users.noreply.github.com>
2026-01-12 12:14:31 +0000

    1stデプロイ+テスト完了

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update, :destroy]
   resources :reviews, only: [:new, :index, :show, :edit, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end

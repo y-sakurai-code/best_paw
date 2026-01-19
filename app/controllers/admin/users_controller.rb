7d4e1c892f9ee7af6fab0befda8fe17845347449
y-sakurai <235877267+y-sakurai-code@users.noreply.github.com>
2026-01-19 11:27:30 +0000

    feature/devise_admin completed

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,4 +7,7 @@ class Admin::UsersController < ApplicationController
         redirect_to admin_dashboards_path, notice: 'ユーザーを削除しました。'
     end
     
+    def show
+      @user = User.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
-    resources :users, only: [:destroy]
+    resources :users, only: [:show, :destroy]
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

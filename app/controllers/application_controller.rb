2f96704f6a898776e77f1316494042f2f2b76c7e
y-sakurai <235877267+y-sakurai-code@users.noreply.github.com>
2026-01-24 08:51:18 +0000

    fix/redirection after sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_authentication
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -10,6 +11,22 @@ class ApplicationController < ActionController::Base
       authenticate_user! unless action_is_public?
     end
   end
+
+  def after_sign_up_path_for(resource)
+    users_mypage_path(resource)
+  end
+
+  def after_sign_in_path_for(resource)
+    users_mypage_path
+  end
+
+  def after_sign_out_path_for(resource)
+    if request.referer&.include?("edit")
+      new_user_registration_path
+    else
+      about_path
+    end
+  end
  
   def admin_controller?
     self.class.module_parent_name == 'Admin'
@@ -17,6 +34,12 @@ class ApplicationController < ActionController::Base
 
   def action_is_public?
     controller_name == 'homes' && (action_name == 'top' || action_name == 'about')
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -26,22 +25,5 @@ class Public::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
-  def after_sign_in_path_for(resource)
-    users_mypage_path
-  end
-
-  def after_sign_out_path_for(resource)
-    if request.referer&.include?("edit")
-      new_user_registration_path
-    else
-      about_path
-    end
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
 
 end

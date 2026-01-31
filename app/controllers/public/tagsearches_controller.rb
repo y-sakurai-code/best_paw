3dad5cd6f7661afbd1d43cfc3c8cc715d7a4a39d
y-sakurai <235877267+y-sakurai-code@users.noreply.github.com>
2026-01-31 08:37:30 +0000

    fix/tagsearches_controller1

--- app/controllers/public/tagsearches_controller.rb
+++ app/controllers/public/tagsearches_controller.rb
@@ -1,9 +1,9 @@
-class TagsearchesController < ApplicationController
+class Public::TagsearchesController < ApplicationController
 
   def search
     @model = Review
     @word = params[:content]
-    @reviews = Review.where("category LIKE?","%#{@word}%")
+    @reviews = Review.where("category LIKE ?", "%#{@word}%").order(created_at: :desc)
     render "tagsearches/tagsearch"
   end
 end

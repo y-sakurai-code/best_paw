45994eaa7761a30f6481337c0ce581a083575e9b
y-sakurai <235877267+y-sakurai-code@users.noreply.github.com>
2026-03-14 04:21:14 +0000

    update/admin_reviews_managements_layout

--- app/controllers/admin/reviews_managements_controller.rb
+++ app/controllers/admin/reviews_managements_controller.rb
@@ -3,7 +3,7 @@ class Admin::ReviewsManagementsController < ApplicationController
   layout 'admin'
 
   def index
-    @reviews = Review.all.order(created_at: :desc)
+    @reviews = Review.all.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def show

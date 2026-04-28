055d80f6b9d7d43116ffb71ec9d5001648873cb0
y-sakurai-code <235877267+y-sakurai-code@users.noreply.github.com>
2026-04-28 20:57:20 +0900

    local_test

--- postcss.config.js
+++ postcss.config.js
@@ -6,10 +6,7 @@ module.exports = {
       autoprefixer: {
         flexbox: 'no-2009'
       },
-      stage: 3,
-      features: {
-        'custom-properties': false
-      }
+      stage: 3
     })
   ]
 }
